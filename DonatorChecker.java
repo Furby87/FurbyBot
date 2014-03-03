79cc437389d7da5fa3744f977f5a47383c429b26
Furby <furby87@unterderbruecke.de>
2014-03-03 09:07:11 +0100

    Update DonatorChecker.java

--- DonatorChecker.java
+++ DonatorChecker.java
@@ -7,9 +7,10 @@ import java.io.*;
 public class DonatorChecker {
 
 		public static String inputLine;
+		public static String top5;
 		
 	    public static void main() throws Exception {
-	        URL urlholder = new URL("https://www.donation-tracker.com/api?mode=3&channel=furby&api_key=xxxxxxxxxxxxxxxxxx&api_lang=de");
+	        URL urlholder = new URL("https://www.donation-tracker.com/api?mode=4&channel=furby&api_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxS&api_lang=de");
 	        URLConnection yc = urlholder.openConnection();
 	        BufferedReader in = new BufferedReader(
 	                                new InputStreamReader(
@@ -20,4 +21,17 @@ public class DonatorChecker {
 
 	        in.close();
 	    }
+	    public static void top5() throws Exception {
+	    	URL urlholder = new URL ("https://www.donation-tracker.com/api?mode=6&channel=furby&api_key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxS&api_lang=de)");
+	    	URLConnection yc = urlholder.openConnection();
+	    	BufferedReader in = new BufferedReader(
+	    						new InputStreamReader(
+	    						yc.getInputStream()));
+	    	
+	    	while ((top5 = in.readLine()) != null)
+	    		MyBot.donatetop5 = top5;
+	    	
+	    	in.close();
+	    } 
+
 	}
